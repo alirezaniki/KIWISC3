51b42eccc1dea92c32ab650e7171919dfb3f427a
Alireza Niksejel <alireza.ns70@gmail.com>
2019-04-03 14:40:28 -0700

    sckiwi_modules

---
+++ sckiwi_init/sckiwi_ki.py
@@ -0,0 +1,20 @@
+import seiscomp3.Kernel, sys, os, subprocess
+# The path to the Source_Analysis file
+file_path = '/home/alireza/seiscomp3/sckiwi/Source_Analysis'
+# File name
+Main_Sc = 'Source_Analysis'
+
+class Module (seiscomp3.Kernel.Module):
+	def __init__ (self, env):
+		seiscomp3.Kernel.Module.__init__(self, env, env.moduleName (__file__))	
+		
+	def _run (self):	
+		if os.path.exists(file_path):
+			params = {}
+			params = self.env.lockFile (self.name)
+			params += " " + "bash %s" % file_path
+			self.env.start (self.name, "run_with_lock", params, True)		
+		else:
+			message = 'The main script < %s > is missing. check SEISCOMP_HOME/sckiwi directory' % (Main_Sc)
+			print (message)	
+	
